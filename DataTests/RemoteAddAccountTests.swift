5c79b42b4a1b85b8dffc46c070997d772a1b7ec0
Cesar <cesarcamposdelima@gmail.com>
2020-03-28 16:02:54 -0300

    Move RemoteAddAccountTests helpers to extension

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -36,6 +36,10 @@ class RemoteAddAccountTests: XCTestCase {
         XCTAssertEqual(httpClientSpy.url, url)
     }
     
+    
+}
+
+extension RemoteAddAccountTests {
     class HttpClientSpy: HttpPostClient {
         var url:URL?
         
