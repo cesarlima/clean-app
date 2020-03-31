d2899041b7b08a3036a727cc33cee84559384ea1
Cesar <cesarcamposdelima@gmail.com>
2020-03-31 19:38:58 -0300

    add infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,17 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by MacPro on 31/03/20.
+//  Copyright © 2020 br.com.cesarlima. All rights reserved.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+
+    func test_() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
