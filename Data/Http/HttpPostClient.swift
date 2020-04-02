07156ae5954e734ddfc68ee9fbde4fbb76898f11
Cesar <cesarcamposdelima@gmail.com>
2020-04-02 10:04:33 -0300

    Move UrlProtocolStub to its own file

---
+++ Data/Http/HttpPostClient.swift
@@ -0,0 +1,13 @@
+//
+//  HttpPostClient.swift
+//  Data
+//
+//  Created by MacPro on 28/03/20.
+//  Copyright © 2020 br.com.cesarlima. All rights reserved.
+//
+
+import Foundation
+
+public protocol HttpPostClient {
+    func post(to url:URL, with:Data?, completion:@escaping (Result<Data?, HttpError>) -> Void)
+}
