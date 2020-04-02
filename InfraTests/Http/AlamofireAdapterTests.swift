d58c9c205fb2780ffcb9b10f7f3309993446516e
Cesar <cesarcamposdelima@gmail.com>
2020-04-02 20:42:33 -0300

    Add CI scheme

--- InfraTests/Http/AlamofireAdapterTests.swift
+++ InfraTests/Http/AlamofireAdapterTests.swift
@@ -49,6 +49,8 @@ class AlamofireAdapterTests: XCTestCase {
         expectResult(.failure(.unauthorized), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 401), error:nil))
         expectResult(.failure(.forbidden), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 403), error:nil))
         expectResult(.failure(.serverError), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 500), error:nil))
+        expectResult(.failure(.noConnectivity), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 300), error:nil))
+        expectResult(.failure(.noConnectivity), when: (data: makeValidData(), response: makeHttpResponse(statusCode: 100), error:nil))
     }
     
     func test_post_should_complete_with_error_on_all_invalid_cases() {
