2841a03478c4c1ab3a0f9ade1cea5b303436b364
Cesar <cesarcamposdelima@gmail.com>
2020-04-03 11:03:28 -0300

    Add presentation layer

---
+++ PresentationTests/SignUpPresenterTests.swift
@@ -0,0 +1,17 @@
+//
+//  PresentationTests.swift
+//  PresentationTests
+//
+//  Created by MacPro on 03/04/20.
+//  Copyright © 2020 br.com.cesarlima. All rights reserved.
+//
+
+import XCTest
+
+class SignUpPresenterTests: XCTestCase {
+    
+    func test_() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
