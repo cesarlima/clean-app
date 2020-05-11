4b4621d6c0ced9206cccae1e258627f9cecdc1ee
Cesar <cesarcamposdelima@gmail.com>
2020-05-11 11:48:10 -0300

    Add UI layer

---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  UITests.swift
+//  UITests
+//
+//  Created by MacPro on 11/05/20.
+//  Copyright © 2020 br.com.cesarlima. All rights reserved.
+//
+
+import XCTest
+
+class SignUpViewControllerTests: XCTestCase {
+
+    func test_() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
