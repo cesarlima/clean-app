5cfd3e6ea280ca8f7b84c1e8c590883c0eb913a6
Cesar <cesarcamposdelima@gmail.com>
2020-05-11 18:55:18 -0300

    SignUpViewController should implements LoadingView protocol

--- UITests/SignUpViewControllerTests.swift
+++ UITests/SignUpViewControllerTests.swift
@@ -7,14 +7,21 @@
 //
 
 import XCTest
+import Presentation
 @testable import UI
 
 class SignUpViewControllerTests: XCTestCase {
 
-    func test_() {
+    func test_loading_is_hidden_on_start() {
         let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
         let sut = sb.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
         sut.loadViewIfNeeded()
         XCTAssertEqual(sut.loadingIndicator?.isAnimating, false)
+    }
+    
+    func test_sut_implements_loadingView() {
+        let sb = UIStoryboard(name: "SignUp", bundle: Bundle(for: SignUpViewController.self))
+        let sut = sb.instantiateViewController(withIdentifier: "SignUpViewController") as! SignUpViewController
+        XCTAssertNotNil(sut as LoadingView)
     }
 }
