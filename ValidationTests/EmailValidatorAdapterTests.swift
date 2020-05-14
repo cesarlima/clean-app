6dc55755654cd2a7d51f6884986146a26a8c0123
Cesar <cesarcamposdelima@gmail.com>
2020-05-14 20:36:35 -0300

    Add validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,16 @@
+//
+//  ValidationTests.swift
+//  ValidationTests
+//
+//  Created by MacPro on 14/05/20.
+//  Copyright © 2020 br.com.cesarlima. All rights reserved.
+//
+
+import XCTest
+
+class ValidationTests: XCTestCase {
+
+    func testExample() {
+        
+    }
+}
